0ddae4705f5ca637f5ab978094a80f26c17de3d3
1980shiet <walsh09chris20@hotmail.com>
2019-10-10 17:00:46 -0700

    add java script

---
+++ js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const elem = document.querySelector('.main-carousel');
+    const flkty = new Flickity(elem, {
+        // options
+        cellAlign: 'left',
+        wrapAround: true,
+        freeScroll: true
+    });
+
+    // all of your code will go here
+
+});
+
+
