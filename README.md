2be90bd2e04d8b7144f01836c98fff524404f933
1980shiet <walsh09chris20@hotmail.com>
2019-11-19 18:36:06 -0800

    Added some javascript for subscribe button and some readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-#Aloha
+<h1>Aloha Project</h1>
+<p>This is the first big project that is required here at Red Academy. The website is based on a fashion company which of course isn't a real one.
 
+<h2> What was used for this project</h2>
+I used Visual code studio, Github, HTML, CSS, Javascript.
+<h3>Made by Chris Walsh.
--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const elem = document.querySelector('.main-carousel');
-    const flkty = new Flickity(elem, {
-        // options
-        cellAlign: 'left',
-        wrapAround: true,
-        freeScroll: true
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  const elem = document.querySelector(".main-carousel");
+  const flkty = new Flickity(elem, {
+    // options
+    cellAlign: "left",
+    wrapAround: true,
+    freeScroll: true
+  });
 
-    // all of your code will go here
-
+  const subbtn = document.getElementById("subscribe-form");
+  console.log(subbtn);
+  subbtn.addEventListener("submit", myFunction);
+  function myFunction(event) {
+    event.preventDefault();
+    //alert("howdy");
+    const email = document.getElementById("email").value;
+    console.log(email);
+  }
 });
-
-
